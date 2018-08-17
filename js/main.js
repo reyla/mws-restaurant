22cc079f1aa391c3fca01d20cd44f693a9eaea81
Kate Speed <katespeed@gmail.com>
2018-08-16 21:36:36 -0400

    Change restaurant details link to Button element

--- js/main.js
+++ js/main.js
@@ -160,11 +160,11 @@ createRestaurantHTML = (restaurant) => {
 
   const imageLink = document.createElement('a');
   imageLink.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(imageLink);
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   image.alt = restaurant.name;
+  li.append(imageLink);
   imageLink.append(image);
 
   const name = document.createElement('h1');
@@ -179,10 +179,11 @@ createRestaurantHTML = (restaurant) => {
   address.innerHTML = restaurant.address;
   li.append(address);
 
-  const more = document.createElement('a');
+  const more = document.createElement('button');
+  more.name = 'View Details About ' + restaurant.name;
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  li.append(more);
 
   return li
 }
